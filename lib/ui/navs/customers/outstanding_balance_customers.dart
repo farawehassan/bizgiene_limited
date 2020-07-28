2362b01665ec2ee2364fd895a41f222c728b7f2c
Farawe Taiwo <farawehassan@yahoo.com>
2020-07-28 08:34:41 +0100

    Commit 1 changes from Moyo PC

--- lib/ui/navs/customers/outstanding_balance_customers.dart
+++ lib/ui/navs/customers/outstanding_balance_customers.dart
@@ -1,123 +1,135 @@
-import 'package:bizgienelimited/utils/reusable_card.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+  import 'package:bizgienelimited/utils/reusable_card.dart';
+  import 'package:flutter/cupertino.dart';
+  import 'package:flutter/material.dart';
+  import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+  import 'package:google_fonts/google_fonts.dart';
 
-class OutstandingBalance extends StatefulWidget {
+  class OutstandingBalance extends StatefulWidget {
 
-  static const String id = 'outstanding_balance_customers_page';
+    static const String id = 'outstanding_balance_customers_page';
 
-  @override
-  _OutstandingBalanceState createState() => _OutstandingBalanceState();
-}
+    @override
+    _OutstandingBalanceState createState() => _OutstandingBalanceState();
+  }
 
-class _OutstandingBalanceState extends State<OutstandingBalance> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 100,
-          margin: const EdgeInsets.all(10.0),
-          child: Material(
-            elevation: 14.0,
-            borderRadius: BorderRadius.circular(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(top: 10.0, left: 12.0, right: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Mr Farawe Taiwo",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Row(
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: (){},
-                            child: Icon(
-                              Icons.update,
-                              color: Colors.grey[500],
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: (){},
-                            child: Icon(
-                              Icons.more_vert,
-                              color: Colors.grey[500],
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 1.0,
-                  color: Colors.grey[500],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 10.0, left: 12.0,),
-                        child: Text(
-                          "Balance - N5,000",
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      child: Container(
-                        width: 2,
-                        child: Text(''),
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 10.0, right: 12.0,),
-                        child: Row(
+  class _OutstandingBalanceState extends State<OutstandingBalance> {
+    @override
+    Widget build(BuildContext context) {
+      return Column(
+        children: <Widget>[
+          Container(
+            height: 100,
+            margin: const EdgeInsets.all(10.0),
+            child: Material(
+              elevation: 14.0,
+              borderRadius: BorderRadius.circular(24.0),
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    height: 49.25,
+                    padding: const EdgeInsets.only(top: 10.0, left: 12.0, right: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
                           children: <Widget>[
-                            GestureDetector(
-                              onTap: (){},
-                              child: Icon(
-                                Icons.calendar_today,
-                                color: Colors.grey[500],
+                            Container(
+                              child: IconButton(
+                                icon: Icon(
+                                  FontAwesomeIcons.solidFile,
+                                ),
+                                onPressed: (){},
+                                color: Color(0xFF008752),
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.green[50]
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 5),
-                              child: Text(
-                                "Tue, 13 Jul",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.normal,
-                                  fontSize: 16,
-                                ),
+                            Text(
+                              "Mr Farawe Taiwo",
+                              style: GoogleFonts.mcLaren(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
                               ),
                             ),
                           ],
                         ),
-                      ),
-                    )
-                  ],
-                )
-              ],
+                        GestureDetector(
+                          onTap: (){},
+                          child: Icon(
+                            Icons.more_vert,
+                            color: Colors.grey[500],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 1.5,
+                    color: Colors.grey[500],
+                  ),
+                  Container(
+                    height: 49.25,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 5.0, left: 12.0,),
+                            child: Text(
+                              "Balance - N5,000",
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.normal,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 1,
+                          height: 49.25,
+                          child: Text(''),
+                          color: Colors.grey,
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 5.0, right: 12.0,),
+                            child: Row(
+                              children: <Widget>[
+                                GestureDetector(
+                                  onTap: (){},
+                                  child: Icon(
+                                    FontAwesomeIcons.calendarAlt,
+                                    color: Colors.grey[400],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 5),
+                                  child: Text(
+                                    "Tue, 13 Jul",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    }
   }
-}
